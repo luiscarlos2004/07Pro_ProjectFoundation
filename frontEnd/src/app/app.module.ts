c84f30eb7ac2a6d7062384d50b0058875b06fc14
Luis Carlos <luiscarlosvaldivieso3@gmail.com>
2022-06-01 18:18:23 -0500

    App cargada

---
+++ frontEnd/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdoptameComponent } from './components/adoptame/adoptame.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdoptameComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
