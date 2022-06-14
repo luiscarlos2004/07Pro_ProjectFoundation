fbdfb1168b8bcfee2e3c49f5f7d0ebc3118af441
Pablo Gonzalez <pablogonzalez1094@gmail.com>
2022-06-14 00:30:09 -0500

    avance en las cards de experiencias

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,11 +1,6 @@
-//npm imports
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
 require('dotenv').config();
 
 const ServerI = require('./models/server');
-
 let serveri = new ServerI();
 
 
