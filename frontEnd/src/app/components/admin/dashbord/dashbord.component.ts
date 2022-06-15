35dcf0ac0fbe64392ec1bc2fee2278f9896b209a
juandiego1426 <juandiegorobayowk@gmail.com>
2022-06-15 18:08:54 -0500

    cambios 50%

--- frontEnd/src/app/components/admin/dashbord/dashbord.component.ts
+++ frontEnd/src/app/components/admin/dashbord/dashbord.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashbord',
   templateUrl: './dashbord.component.html',
-  styleUrls: ['./dashbord.component.css']
+  styleUrls: ['./dashbord.component.css','../../../../../fontAwesome/css/all.min.css','../../../../styles.css']
 })
 export class DashbordComponent implements OnInit {
 
